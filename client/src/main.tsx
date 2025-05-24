6bb2b1d7bcca4411c0e2675430165d0f214528c4
difwahab <difwahab@msn.com>
2025-05-24 00:30:29 -0400

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,27 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+
 import { LanguageProvider } from "./context/LanguageContext";
 import { AuthProvider } from "./context/AuthContext";
+
 import "./lib/i18n";
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </AuthProvider>
-  </QueryClientProvider>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+} else {
+  console.error("Impossible de trouver l'élément root dans le DOM.");
+}
