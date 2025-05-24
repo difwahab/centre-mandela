2a88867c796c286a13039686f3816e15b916ff74
difwahab <difwahab@msn.com>
2025-05-24 05:29:49 -0400

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import Hero from './components/home/Hero';
-import About from './components/home/About';
-import Services from './components/home/Services';
-import Equipment from './components/home/Equipment';
-import Appointment from './components/home/Appointment';
-import DoctorPortal from './components/home/DoctorPortal';
-import News from './components/home/News';
-import Testimonials from './components/home/Testimonials';
-import Contact from './components/home/Contact';
+import Hero from '@/components/home/Hero';
+import About from '@/components/home/About';
+import Services from '@/components/home/Services';
+import Equipment from '@/components/home/Equipment';
+import Appointment from '@/components/home/Appointment';
+import DoctorPortal from '@/components/home/DoctorPortal';
+import News from '@/components/home/News';
+import Testimonials from '@/components/home/Testimonials';
+import Contact from '@/components/home/Contact';
 
 const Home = () => {
   const { t } = useTranslation();
