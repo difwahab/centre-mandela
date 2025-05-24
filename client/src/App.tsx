ef296dad1f257b624ab0439ee0cfabc6556a0290
difwahab <difwahab@msn.com>
2025-05-24 03:19:17 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
